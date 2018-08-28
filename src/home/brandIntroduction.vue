1bc93bc63bed03a3c0674528a42c1341eba13644
DESKTOP-9Q0Q2MH\云风小姐姐 <623691692@qq.com>
2018-08-28 16:37:48 +0800

    品牌简介，html纯格式 未改element

---
+++ src/home/brandIntroduction.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="brandIntroduction">
+        <div class="header">
+            <h1>颜墨映像 studio</h1>
+        </div>
+        <div class="main">
+            <img src="../assets/brandIntroduction/mainimg.jpg">
+        </div>
+        <div class="footer">
+            <span>颜墨映像 Studio－<br>秉承记录生活的自然感,找寻自然的美。追求真实。<br>一群奔走各个城市的80后,从业十年。<br>直至在此安坐,十余年来，我們珍视这样的共鸣：<br>恰如珍藏一本有人知的好书，一份从未相见的默契。<br>期待与你／祢 相遇。</span><br>
+        </div>
+    </div>
+</template>
+
+<style>
+    .brandIntroduction {
+        width: 1366px;
+        height: 702px;
+        background: url('../assets/brandIntroduction/backimg.jpg');
+    }
+    .header {
+        width: 1000px;
+        height: 75px;
+        margin: 0 auto;
+    }
+    h1{
+        text-align: center;
+        margin: 0 48px 0 0;
+        padding: 0;
+        font-weight: 700;
+        font-family: 'Open Sans Light';
+        font-size: 40px;
+        line-height: 100px;
+        font-family:'Courier New', Courier, monospace;
+        color: rgba(77,77,77,1)
+    }
+    .main {
+        margin-top: 55px;
+        text-align: center
+    }
+    .main img{
+        max-width: 100%;
+        width: 250px;
+        height: 250px;
+        border-radius: 100%;
+    }
+    .footer {
+        line-height: 40px;
+        margin-top: 48px;
+        text-align: center;
+        color:rgba(105,105,105,1);
+        font-weight: 700;
+        font-family:'Courier New', Courier, monospace;
+        font-size: 20px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/home/home'
-import photos from '@/photos/photos'
-import cameramen from '@/cameramen/cameramen'
-import paymentPage from '@/paymentPage/paymentPage'
+import brandIntroduction from '@/home/brandIntroduction'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // 首页
     {
       path: '/',
       name: 'home',
       component: home
     },
-    // 作品展示页面
     {
-      path: '/photos',
-      name: 'photos',
-      component: photos
-    },
-    // 摄影师页面
-    {
-      path: '/cameramen',
-      name: 'cameramen',
-      component: cameramen
-    },
-    // 支付页面
-    {
-      path: 'paymentPage',
-      name: 'paymentPage',
-      component: paymentPage
+      path: '/brandIntroduction',
+      name: 'brandIntroduction',
+      component: brandIntroduction
     }
   ]
 })
