4a6b8a4d701677a131ad79098de6b935245e2188
PC-201807311322\Administrator <jai15032567795@163.com>
2018-08-30 11:11:03 +0800

    修改一下

--- src/gravida/gravida.vue
+++ src/gravida/gravida.vue
@@ -1,9 +1,9 @@
 <template>
-    <h3>孕妇写真</h3>
-    <img src="../assets/孕妇1.jpg">
-    <img src="../assets/孕妇2.jpg">
-    <img src="../assets/孕妇3.jpg">
-    <img src="../assets/孕妇4.jpg">
+      <h3>孕妇写真</h3>
+      <img src="../assets/pregnancy/孕妇1.jpg">
+      <img src="../assets/pregnancy/孕妇2.jpg">
+      <img src="../assets/pregnancy/孕妇3.jpg">
+      <img src="../assets/pregnancy/孕妇4.jpg">
 </template>
 
 <script>
